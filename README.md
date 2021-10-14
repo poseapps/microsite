cb6c2ec72681b567a29c666a3b353acbf45ff738
THEGamingninja <thegamingninjamj@gmail.com>
2021-10-14 17:27:25 -0400

    Initial Version

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# microsite
+# After Pose Microsite
+[![Netlify Status](https://api.netlify.com/api/v1/badges/76aa3925-d141-42c8-8923-916953342163/deploy-status)](https://app.netlify.com/sites/afterpose/deploys)
+
+This is a Netlify serverless function for displaying After Pose images from the After Pose API using express as a webserver and ejs for templating.
+
+Deployment: [https:photos.afterpose.com](https:photos.afterpose.com)
+
+Routes
+===
+`/` - Redirects to [https://poseapps.com](https://poseapps.com)
+
+`/:userId/:photoId` - Displays Photo and Microsite
---
+++ express/server.js
@@ -0,0 +1,53 @@
+'use strict'
+const ejs = require('ejs');
+const axios = require('axios').default;
+const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
+
+const API_URL = process.env["API_URL"] ?? "https://my.afterpose.com/api/v1";
+const CDN_URL = process.env["CDN_URL"] ?? "https://my.afterpose.com";
+
+const http = axios.create({
+  baseURL: API_URL,
+})
+
+const router = express.Router();
+router.get('/:uid/:photoId', async (req, res) => {
+  try {
+    const response = await http.get(`/microsite/${req.params.uid}/${req.params.photoId}`)
+    const fullUrl = req.protocol + '://' + req.hostname + req.originalUrl;
+    const rendered = await ejs.renderFile("./views/index.ejs", {
+      location: fullUrl,
+      contentUrl: CDN_URL,
+      ...response.data
+    });
+
+    res.writeHead(200, { 
+      'Content-Type': 'text/html'
+    });
+    res.write(rendered);
+  }
+  catch {
+    res.writeHead(200, { 
+      'Content-Type': 'text/plain'
+    });
+    res.write("There was an error loading your after pose")
+  }
+
+  res.end();
+})
+
+router.get('/:uid/:photoId/echo', async (req, res) => {
+  res.writeHead(200, { 
+    'Content-Type': 'text/plain'
+  });
+  res.write(`${req.params.uid} - ${req.params.photoId}`);
+  res.end();
+})
+
+app.use('/', router);
+app.use(express.static("public"))
+
+module.exports = app;
+module.exports.handler = serverless(app);
